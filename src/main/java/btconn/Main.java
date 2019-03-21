c53097d19450f1428d8c7fbf94f24d19439cd788
Unknown <unknown@example.com>
2019-03-21 12:56:09 +0100

    front stuff

--- src/main/java/btconn/Main.java
+++ src/main/java/btconn/Main.java
@@ -2,6 +2,9 @@ package btconn;
 
 import btconn.bluetooth.ConnectionBT;
 import btconn.database.ConnectionDB;
+import btconn.database.UserCommand;
+
+import java.util.ArrayList;
 
 public class Main {
 
@@ -10,7 +13,17 @@ public class Main {
         ConnectionBT connectionBT = new ConnectionBT("This is my fancy BT connection steen bij 101");
         try{
             ConnectionDB connectionDB = new ConnectionDB();
-            connectionDB.executeQuery("SELECT * FROM `users`");
+//            connectionDB.inserQuery("INSERT INTO `user_command`(`user_id`, `cmd_id`) VALUES (%d, '%s') ", 9981, "w");
+//            connectionDB.inserQuery("INSERT INTO `user_command`(`user_id`, `cmd_id`) VALUES (%d, '%s') ", 9981, "a");
+//            connectionDB.inserQuery("INSERT INTO `user_command`(`user_id`, `cmd_id`) VALUES (%d, '%s') ", 9981, "s");
+//            connectionDB.inserQuery("INSERT INTO `user_command`(`user_id`, `cmd_id`) VALUES (%d, '%s') ", 9981, "d");
+            while(true) {
+                ArrayList<UserCommand> userCommands = connectionDB.executeQueryus("SELECT * FROM `user_command`");
+                for (UserCommand userCommand : userCommands) {
+                    System.out.println(userCommand.toString() + "\n");
+                    connectionDB.delete(userCommand.getId());
+                }
+            }
         }catch (Exception ex) {
             ex.printStackTrace();
         }
--- src/main/java/btconn/database/ConnectionDB.java
+++ src/main/java/btconn/database/ConnectionDB.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 public class ConnectionDB {
@@ -23,8 +24,33 @@ public class ConnectionDB {
         ResultSet rs = stmt.executeQuery(sql);
         while(rs.next())
         {
+
             System.out.println("Respomse: " + rs.getString("username") + " " + rs.getString("pass"));
         }
     }
 
+    public void inserQuery(String sql, int id, String cmd) throws Exception
+    {//String.format(sql, 9981, 'w'))
+        stmt.execute(String.format(sql, id, cmd));
+    }
+
+
+    public ArrayList<UserCommand> executeQueryus(String sql) throws Exception
+    {
+        ArrayList<UserCommand> userCommands = new ArrayList<UserCommand>();
+        ResultSet rs = stmt.executeQuery(sql);
+        while(rs.next())
+        {
+            UserCommand userCommand = new UserCommand(Integer.parseInt(rs.getString("ID")),
+                                                Integer.parseInt(rs.getString("user_id")),
+                                                rs.getString("cmd_id"));
+            userCommands.add(userCommand);
+        }
+        return userCommands;
+    }
+
+    public void delete(int cmdId) throws Exception
+    {
+        stmt.execute(String.format("DELETE FROM `user_command` WHERE `id` = %d", cmdId));
+    }
 }
