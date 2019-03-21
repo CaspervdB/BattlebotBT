76d2e79cb2281f13dde77f6b143fc23b5d1df93b
Casper <cms.vandenberg@gmail.com>
2019-03-21 10:25:04 +0100

    Bluetoothstuff

---
+++ src/main/java/btconn/ConnectionBT.java
@@ -0,0 +1,21 @@
+package btconn;
+
+public class ConnectionBT {
+
+    private String conn;
+    private String command;
+    public ConnectionBT(String conn)
+    {
+        this.conn = conn;
+        command = "This is my command";
+    }
+
+    public String getConn(){
+        return conn;
+    }
+
+    public String getCommand()
+    {
+        return command;
+    }
+}
---
+++ src/main/java/btconn/Main.java
@@ -0,0 +1,17 @@
+package btconn;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        ConnectionBT connectionBT = new ConnectionBT("This is my fancy BT connection steen bij 101");
+        try{
+            ConnectionDB connectionDB = new ConnectionDB();
+            connectionDB.executeQuery("SELECT * FROM `users`");
+        }catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        System.out.println(connectionBT.getConn());
+    }
+}
