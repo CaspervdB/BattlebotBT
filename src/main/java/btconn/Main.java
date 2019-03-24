cdbdf3286fc2e4461cbee9cd5a9ca5cc971043b3
codrut51 <codrutsoft13@gmail.com>
2019-03-24 19:22:44 +0100

    Database + refactor main

--- src/main/java/btconn/Main.java
+++ src/main/java/btconn/Main.java
@@ -1,31 +1,46 @@
 package btconn;
 
 import btconn.bluetooth.ConnectionBT;
+import btconn.bluetooth.ListenerBT;
 import btconn.database.ConnectionDB;
 import btconn.database.UserCommand;
 
 import java.util.ArrayList;
 
 public class Main {
-
     public static void main(String[] args)
     {
         try{
             ConnectionDB connectionDB = new ConnectionDB();
-//            connectionDB.inserQuery("INSERT INTO `user_command`(`user_id`, `cmd_id`) VALUES (%d, '%s') ", 9981, "w");
-//            connectionDB.inserQuery("INSERT INTO `user_command`(`user_id`, `cmd_id`) VALUES (%d, '%s') ", 9981, "a");
-//            connectionDB.inserQuery("INSERT INTO `user_command`(`user_id`, `cmd_id`) VALUES (%d, '%s') ", 9981, "s");
-//            connectionDB.inserQuery("INSERT INTO `user_command`(`user_id`, `cmd_id`) VALUES (%d, '%s') ", 9981, "d");
-            while(true) {
-//                ArrayList<UserCommand> userCommands = connectionDB.executeQueryus("SELECT * FROM `user_cmd`");
-//                for (UserCommand userCommand : userCommands) {
-//                    System.out.println(userCommand.toString() + "\n");
-//                    connectionDB.delete(userCommand.getId());
-//                }
+            ListenerBT bt = new ListenerBT(connectionDB.getBluetoothNames());
+            ConnectionBT bluetoothConn = new ConnectionBT(bt);
+
+            bluetoothConn.startDiscovery();
+            System.out.println("Discovering devices has started");
+
+            while(bluetoothConn.hasDiscoveredDevices())
+            {
+                try{
+                    Thread.sleep(500);
+                }catch (Exception ex){
+
+                }
             }
-        }catch (Exception ex) {
+            System.out.println("\nThe program found: " + bluetoothConn.getDevices().size() +"\n");
+            System.out.println("Program ended!");
+            bluetoothConn.createConnections();
+            while(true)
+            {
+                ArrayList<UserCommand> usrCmds = connectionDB.getCommands();
+                for(UserCommand usrCmd : usrCmds)
+                {
+                    bluetoothConn.sendCommands(usrCmd.getUserid(), usrCmd.getCommand());
+                    connectionDB.delete(usrCmd.getId());
+                }
+            }
+        }catch (Exception ex)
+        {
             ex.printStackTrace();
         }
-
     }
 }
